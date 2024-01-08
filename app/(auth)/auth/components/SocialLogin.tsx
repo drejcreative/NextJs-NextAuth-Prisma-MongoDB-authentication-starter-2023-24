0db412f75914ef5da3a5f92918aaedf7b5a5699b
Andrej Vajagic <drejcreative@gmail.com>
2024-01-08 14:19:00 +0100

    Change loader

--- app/(auth)/auth/components/SocialLogin.tsx
+++ app/(auth)/auth/components/SocialLogin.tsx
@@ -12,12 +12,13 @@ const SocialLogin = () => {
     <div className="mt-12 grid gap-6 sm:grid-cols-2">
       <button className="rounded-xl bg-blue-50 px-6 py-3 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200">
         <div className="flex justify-center" onClick={() => socialActions('google')}>
-          {loadingGoogle ? <Loading /> : <FcGoogle size={20} className="mr-2" />}
+          {loadingGoogle ? <Loading className="mr-2" /> : <FcGoogle size={20} className="mr-2" />}
           <span className="block w-max text-sm font-medium tracking-wide text-blue-700">
             Google
           </span>
         </div>
       </button>
+
       <button className="rounded-xl bg-gray-900 px-6 py-3 transition hover:bg-gray-800 focus:bg-gray-700 active:bg-gray-600">
         <div
           className="flex items-center justify-center text-white"
